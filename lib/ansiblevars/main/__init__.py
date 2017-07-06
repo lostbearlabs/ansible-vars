8b9264e9ecc395f7e98ba3bac289129c95ac48fc
Eric Johnson <eric3517@icloud.com>
2017-07-06 17:31:44 -0500

    move reporting to new Report class

--- lib/ansiblevars/main/__init__.py
+++ lib/ansiblevars/main/__init__.py
@@ -2,29 +2,14 @@ import sys
 import errno
 
 from ansiblevars.project import Project
+from ansiblevars.report import Report
 
 
 def main():
     path = "/Users/eric/dev/ansible/ansible-hydra"
     project = Project(path)
-    for playbook in sorted(project.get_playbooks(), key=lambda p: p.get_name()):
-        report("Playbook", playbook.get_name(), playbook)
-    for role in sorted(project.get_roles(), key=lambda r: r.get_role_name()):
-        report("Role", role.get_role_name(), role)
-
-
-def report(tag, name, book):
-    print("%s %s:" % (tag, name))
-    print("   defaults:")
-    defaults = book.get_defaults();
-    if defaults:
-        for dx in sorted(defaults, key=lambda p: p.get_variable_name()):
-            print("      %s" % dx)
-    references = book.get_references()
-    print("   references:")
-    if references:
-        for rx in sorted(references, key=lambda p: p.get_variable_name()):
-            print("      %s" % rx)
+    report = Report(project)
+    report.display()
 
 
 if __name__ == "__main__":
